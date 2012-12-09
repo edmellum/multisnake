3f76a13c6be8a798cd24c942bce09c638eb6ebe2
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2012-12-09 21:26:17 +0100

    Added socket io to client

--- public/js/client.js
+++ public/js/client.js
@@ -6,4 +6,9 @@ $(document).ready(function() {
 	else {
 		//displayMessage("Your browser does not seem to support HTML5 canvas...");
 	}
+
+	var socket = io.connect('http://localhost');
+	socket.on('players', function(players) {
+		console.log(players);
+	})
 });
