6d1d959f636f2a687d187cf72460ea8ddf0b6ded
David Ed Mellum <david@edmellum.com>
2012-12-09 21:08:09 +0100

    Add socket.io to server

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var http = require('http');
 
+var io = require('socket.io');
 var ecstatic = require('ecstatic');
 
-http.createServer(
-	ecstatic({root: __dirname + '/public'})
-).listen(8080);
+var players = [];
+
+var fileServer = ecstatic({root: __dirname + '/public'});
+var httpServer = http.createServer(fileServer);
+
+io.listen(httpServer);
+httpServer.listen(8080);
+
+io.sockets.on('connection', function(socket) {
+	socket.emit('players', players);
+});
