7bc09a9baa3e9fa86500688a0589c5e9ba009358
David Ed Mellum <david@edmellum.com>
2012-12-09 21:54:09 +0100

    Implement player updating server-side

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var socketio = require('socket.io');
 var ecstatic = require('ecstatic');
 
-var players = [];
+var players = {};
 
 var fileServer = ecstatic({root: __dirname + '/public'});
 var httpServer = http.createServer(fileServer);
@@ -12,6 +12,14 @@ var io = socketio.listen(httpServer);
 httpServer.listen(8080);
 
 io.sockets.on('connection', function(socket) {
-	players.push(socket.id);
+	players[socket.id] = {};
 	socket.emit('players', players);
+
+	socket.on('update', function(player) {
+		io.sockets.emit('update', player);
+	});
+
+	socket.on('disconnect', function() {
+		delete players[socket.id];
+	});
 });
