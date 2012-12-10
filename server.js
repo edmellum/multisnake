6f4f4e6d3d59bdf4115038e2d34c98168c3b142a
Kjetil Barnung <kjetil.barnung@knowit.no>
2012-12-10 19:46:30 +0100

    added highscore functionality

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var socketio = require('socket.io');
 var ecstatic = require('ecstatic');
 
 var players = {};
+var scores = [];
+var maxScores = 2;
 
 var fileServer = ecstatic({root: __dirname + '/public'});
 var httpServer = http.createServer(fileServer);
@@ -19,7 +21,33 @@ io.sockets.on('connection', function(socket) {
 		io.sockets.emit('update', player);
 	});
 
+	socket.on('score', function(score) {
+		if (scores.length < maxScores){
+			scores.push(score);
+		}
+		else
+		{
+			var minScore = scores[scores.length - 1].score;
+			if(score.score > minScore)
+			{
+				scores[scores.length -1] = score;	
+			}
+		}
+		scores = sortResults();
+		console.log("SCORE @" + score.score + " for " + score.name);
+		io.sockets.emit('highscores', scores);
+	});
+
 	socket.on('disconnect', function() {
 		delete players[socket.id];
 	});
 });
+Array.prototype.min = function() {
+	var min = this[0].score;
+	var len = this.length;
+	for (var i = 1; i < len; i++) if (this[i] < min) min = this[i];
+	return min;
+}
+function sortResults(){
+	return scores.sort(function(b,a) { return parseInt(a.score) - parseInt(b.score) } );
+}
